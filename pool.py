fed680dfc9beef331e495cd2418eed323bdcdcf8
alexsuse <alexsusemihl@gmail.com>
2012-08-09 16:39:42 +0200

    pool fixed, maybe

--- pool.py
+++ pool.py
@@ -26,7 +26,7 @@ def runPF(alpha):
 	
 	#code is the population of neurons, plastic poisson neurons	
 	code_rng = np.random.mtrand.RandomState()
-	code = pn.PoissonPlasticCode(A=alpha,phi=phi/2,tau=tau,thetas=np.arange(-20.0,20.0,0.15),dm=dm,randomstate=code_rng)
+	code = pn.PoissonPlasticCode(A=alpha,phi=phi/2,tau=tau,thetas=np.arange(-20.0,20.0,0.15),dm=dm,randomstate=code_rng,alpha=alpha)
 	
 	#s is the stimulus, sps holds the spikes, rates the rates of each neuron and particles give the position of the particles
 	#weights gives the weights associated with each particle
