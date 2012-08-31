234e82a911afc431d5624ee9fd2fa804394d31bc
alexsuse <alexsusemihl@gmail.com>
2012-08-31 16:45:18 +0200

    pool_ratedistortion should give same data as ratedistortino now...

--- pool_ratedistortion.py
+++ pool_ratedistortion.py
@@ -9,17 +9,21 @@ import cPickle as pic
 import os
 import sys
 
+gamma = 1.0
+eta = 1.0
+alpha = 1.0
+delta = 0.1
+tau = 1.0
+thetas = np.arange(-4.0,4.0,0.2)
+
 dt = 0.001
 zeta = 1.0
-eta = 1.8
-gamma = 1.2
 timewindow = 1000000
-dm = 0.2
 nparticles = 200
 alpha = 0.7
 
 def runPF(params):
-	[phi,tau] = params
+	[phi,delta] = params
 	env_rng = np.random.mtrand.RandomState()
 	
 	env = ge.GaussianEnv(gamma=gamma,eta=eta,zeta=zeta,x0=0.0,y0=.0,L=1.0,N=1,order=1,sigma=0.1,Lx=1.0,Ly=1.0,randomstate=env_rng)
@@ -27,7 +31,7 @@ def runPF(params):
 	
 	#code is the population of neurons, plastic poisson neurons	
 	code_rng = np.random.mtrand.RandomState()
-	code = pn.PoissonPlasticCode(A=alpha,phi=phi,tau=tau,thetas=np.arange(-10.0,10.0,0.15),dm=dm,randomstate=code_rng,alpha=alpha)
+	code = pn.PoissonPlasticCode(A=alpha,phi=phi,tau=tau,thetas=thetas,dm=delta,randomstate=code_rng,alpha=alpha)
 	
 	#s is the stimulus, sps holds the spikes, rates the rates of each neuron and particles give the position of the particles
 	#weights gives the weights associated with each particle
@@ -39,36 +43,52 @@ def runPF(params):
 	code.reset()
 	
 	[mmse,spikecount] = pf.mse_particle_filter(code,env,timewindow=timewindow,dt=dt,nparticles=nparticles,mode = 'Silent')
-	print "ping "+str(alpha)+" "+str(tau)+" "+str(mmse)+" "+str(spikecount)
-	return [phi,tau,mmse, spikecount]
+	print "ping "+str(phi)+" "+str(delta)+" "+str(mmse)+" "+str(spikecount)
+	return [phi,delta,mmse, spikecount]
 
 if __name__=='__main__':
-	phis = np.arange(0.1,20.0,0.4)
-	taus = np.arange(0.001,11.0,2.0)
+	#parameters for running
+	deltas = np.arange(0.0,0.6,0.1)
+	phis = np.arange(0.0,100.0,0.5)
+
+#pool initalization
 	ncpus = mp.cpu_count()
 	pool = Pool(processes= ncpus)
-	params = [[p,t] for p in phis for t in taus]
+
+#parmeters
+	params = [[p,d] for p in phis for d in delta]
+
+#DO IT, DO IT NOW!
 	outp = pool.map(runPF,params)
-	mmse = np.zeros((phis.size,taus.size))
+
+#Post processing
 	os.system("""echo "post-processing now..."|mail -s "Simulation" alexsusemihl@gmail.com""")
+
+#initialize dictionaries and arrays
 	mmsedic = {}
 	spikedic = {}
 	nphis = phis.size
-	ntaus = taus.size
-	mmse = np.zeros((nphis,ntaus))
-	spcount = np.zeros((nphis,ntaus))
+	ndeltas = taus.size
+	mmse = np.zeros((nphis,ndeltas))
+	spcount = np.zeros((nphis,ndeltas))
+
+#parse pool output
 	for o in outp:
-		[phi,tau,rest,spikec] = o
-		mmsedic[(phi,tau)] = rest
-		spikedic[(phi,tau)] = spikec
+		[phi,delta,rest,spikec] = o
+		mmsedic[(phi,delta)] = rest
+		spikedic[(phi,delta)] = spikec
+
+#store it in arrays
 	for i,p in enumerate(phis):
-		for j,t in enumerate(taus):
+		for j,t in enumerate(deltas):
 			mmse[i,j] = mmsedic[(p,t)]
 			spcount[i,j] = spikedic[(p,t)]
+
+#print it, pickle it, pack it, technologic
 	if len(sys.argv)>1:
 		filename = sys.argv[1]
 	else:
 		filename = "../data/pickle_alphas_1"
 	fi= open(filename,'w')
-	pic.dump([mmse,spcount,alpha,taus],fi)
+	pic.dump([mmse,spcount,phis,deltas],fi)
 	os.system("""echo "simulation is ready, dude!"|mail -s "Simulation" alexsusemihl@gmail.com""")
