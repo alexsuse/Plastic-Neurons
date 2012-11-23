8ed149265cebab3069d36659f1b5ba41bb617177
alexsuse <alexsusemihl@gmail.com>
2012-11-23 17:45:35 +0100

    still playing around

--- lateralinhibition.py
+++ lateralinhibition.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class lateralInhibitionCode(object):
 	def __init__(self,alpha,phi,tau,zeta,thetas):
@@ -15,10 +16,10 @@ class lateralInhibitionCode(object):
 		self.rng = np.random.mtrand.RandomState(123456)
 
 	def inhibitionKernel(self,i):
-		return np.exp(-0.5*(self.thetas-self.thetas[i])**2/self.zeta)
+		return np.exp(self.zeta*(-1.0+np.cos(self.thetas-self.thetas[i])))
 
 	def spike(self,X,dt):
-		rates = self.kappas*np.exp(-0.5*(self.thetas-X)**2/self.alpha**2)*self.phi
+		rates = self.kappas*np.exp(self.alpha*(-1.0+np.cos(self.thetas-X)))*self.phi
 		totalRate = dt*np.sum(rates)
 		if self.rng.rand()<totalRate:
 			#choose which neuron spiked
@@ -55,19 +56,52 @@ def choice(p,a=None,shape=(1,),randomstate = None):
 		return a[idxs]
 
 if __name__=="__main__":
-	inhibCode = lateralInhibitionCode(alpha=0.5,phi=1.0,tau=1.0,thetas = np.arange(-4.0,4.0,0.1),zeta = 0.9)
+	nneurons = 10
+	neurons = np.arange(-np.pi,np.pi,2*np.pi/nneurons)
+	alpha=0.9
+	phi = 100.0
+	tau = 1.0
+	zeta = 0.4
+	inhibCode = lateralInhibitionCode(alpha=alpha,phi=phi,tau=tau,thetas = neurons,zeta = zeta)
 	dt = 0.001
-	xs = np.arange(-2.0,2.0,0.2)
+	Ntrials = 1000000
+	xs = np.arange(-np.pi,np.pi,0.5*np.pi/nneurons)
+	ps = np.ones(shape = xs.shape)
+	ps[nneurons/2] = ps[nneurons/2] + 0.8
+	ps[0] = ps[0]+0.4
+	ps = ps/np.sum(ps)
 	bins = np.arange(-0.5,len(inhibCode.thetas)+0.5,1.0)
 	results = {}
 	histograms = {}
+	for n in neurons:
+		histograms[n] = []
 	for x in xs:
-		inhibCode.reset()
 		results[x]=[]
-		for i in range(10000):
-			spike = inhibCode.spike(x,dt)
-			if spike:
-				results[x].append(spike)
+	for i in range(Ntrials):
+		print i/np.float(Ntrials)
+		x = choice(ps,a=xs)[0]
+		spike = inhibCode.spike(x,dt)
+		if spike!=None:
+			spike= spike[0]
+			results[x].append(spike)
+			histograms[neurons[spike]].append(x)
+	for x in xs:
 		results[x] =  np.array(results[x]).ravel()
-		histograms[x] = np.histogram(results[x],bins=bins)
+	for x in neurons:
+		histograms[x] = np.array(histograms[x]).ravel()
+
+	hists = np.zeros(shape=(nneurons,2*nneurons))
+	bins = np.arange(-np.pi,np.pi+0.01,np.pi/nneurons)
+	for i,x in enumerate(neurons):
+		print len(histograms[x])
+		hists[i,:] = np.histogram(histograms[x],bins=bins,normed=True)[0]
+
+	for i,x in enumerate(neurons):
+		plt.plot(bins[:-1],hists[i,:])
+		plt.plot(bins[:-1],np.exp(alpha*(-1.0+np.cos(bins[:-1]-x))))
+		plt.show()
+
+	#plt.imshow(hists,interpolation='nearest')
+	#plt.show()
+	#histograms[x] = np.histogram(results[x],bins=bins)
 
