c808a2e2e961fe637aa84439860f4a3993b266f5
alexsuse <alexsusemihl@gmail.com>
2014-07-15 15:40:18 +0200

    multiprocessing

--- dense_to_sparse.py
+++ dense_to_sparse.py
@@ -1,4 +1,4 @@
-#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+#/!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 """Particle filtering for nonlinear dynamic systems observed through adaptive poisson neurons"""
 from prettyplotlib import plt
 import prettyplotlib as ppl
@@ -8,33 +8,11 @@ import poissonneuron as pn
 import numpy as np
 import cPickle as pic
 import sys
+import multiprocessing as mp
 
 #parameter definitions
 
-dt = 0.001
-phi = 1.0
-zeta = 1.0
-eta = 1.0
-gamma = 1.0
-alpha = 0.5
-timewindow = 100000
-dm = 0.0
-tau = 1.0
-nparticles = 600
-plotting = True
-
 #env is the "environment", that is, the true process to which we don't have access
-
-env_rng = np.random.mtrand.RandomState()
-
-env = ge.GaussianEnv(gamma=gamma,eta=eta,zeta=zeta,x0=0.0,
-                     y0=.0,L=1.0,N=1,order=1,sigma=0.1,
-                     Lx=1.0,Ly=1.0,randomstate=env_rng)
-env.reset(np.array([0.0]))
-
-#code is the population of neurons, plastic poisson neurons
-
-code_rng = np.random.mtrand.RandomState()
 
 #s is the stimulus, sps holds the spikes, rates the rates of each neuron
 #and particles give the position of the particles
@@ -45,6 +23,62 @@ dthetas = np.arange(0.1,2.0,0.1)
 sparse_eps = np.zeros((dthetas.size,))
 dense_eps = np.zeros((dthetas.size,))
 particle_eps = np.zeros((dthetas.size,))
+
+def run_filters(arg):
+    i,dtheta = arg
+
+    dt = 0.001
+    phi = 1.0
+    zeta = 1.0
+    eta = 1.0
+    gamma = 1.0
+    alpha = 0.5
+    timewindow = 1000000
+    dm = 0.0
+    tau = 1.0
+    nparticles = 1000
+
+    env_rng = np.random.mtrand.RandomState()
+
+    env = ge.GaussianEnv(gamma=gamma,eta=eta,zeta=zeta,x0=0.0,
+                         y0=.0,L=1.0,N=1,order=1,sigma=0.1,
+                         Lx=1.0,Ly=1.0,randomstate=env_rng)
+    env.reset(np.array([0.0]))
+
+    #code is the population of neurons, plastic poisson neurons
+
+    code_rng = np.random.mtrand.RandomState()
+    
+    code = pn.PoissonPlasticCode(A=alpha,phi=phi/2,tau=tau,
+                                 thetas=np.arange(-20.0,20.0,dtheta),
+                                 dm=dm,randomstate=code_rng)
+    
+    env_rng.seed(12345)
+    code_rng.seed(67890)
+    env.reset(np.array([0.0]))
+    code.reset()
+    
+    [densem,densevar,spsg,sg,dense_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
+                                                            dense=True)
+    
+    env_rng.seed(12345)
+    code_rng.seed(67890)
+    env.reset(np.array([0.0]))
+    code.reset()
+    
+    [sparsem,sparsevar,spsg,sg,sparse_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
+                                                                dense=False)
+    
+    env_rng.seed(12345)
+    code_rng.seed(67890)
+    env.reset(np.array([0.0]))
+    code.reset()
+    
+    [msep,ws] = pf.mse_particle_filter(code, env, timewindow=timewindow,
+                                                         dt=dt, nparticles=nparticles,
+                                                         testf=(lambda x:x))
+
+    return i,dense_mse,sparse_mse,msep
 
 try:
     filename = sys.argv[1]
@@ -61,41 +95,14 @@ try:
 
 except:
 
-    for i,dtheta in enumerate(dthetas):
-        print i,dthetas.size
-        
-        code = pn.PoissonPlasticCode(A=alpha,phi=phi/2,tau=tau,
-                                     thetas=np.arange(-20.0,20.0,dtheta),
-                                     dm=dm,randomstate=code_rng)
-        
-        env_rng.seed(12345)
-        code_rng.seed(67890)
-        env.reset(np.array([0.0]))
-        code.reset()
-        
-        [densem,densevar,spsg,sg,dense_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
-                                                                dense=True)
-        
-        env_rng.seed(12345)
-        code_rng.seed(67890)
-        env.reset(np.array([0.0]))
-        code.reset()
-        
-        [sparsem,sparsevar,spsg,sg,sparse_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
-                                                                    dense=False)
-        
-        env_rng.seed(12345)
-        code_rng.seed(67890)
-        env.reset(np.array([0.0]))
-        code.reset()
-        
-        [msep,ws] = pf.mse_particle_filter(code, env, timewindow=timewindow,
-                                                             dt=dt, nparticles=nparticles,
-                                                             testf=(lambda x:x))
+    p = mp.Pool(mp.cpu_count())
+    results = p.map(run_filters,zip(range(dthetas.size),dthetas))
 
-        dense_eps[i] = dense_mse
-        sparse_eps[i] = sparse_mse
-        particle_eps[i] = msep
+    for r in results:
+        i,dense,sparse,part = r
+        dense_eps[i] = dens
+        sparse_eps[i] = sparse
+        particle_eps[i] = part
 
     with open(filename,"wb") as f:
         pic.dump({'dense_eps':dense_eps,
@@ -109,8 +116,10 @@ if plotting:
     #matplotlib.rcParams['font.size']=10
     fig, ax = ppl.subplots(1,figsize=(5,5))
 
-    ppl.plot(dthetas,sparse_eps,ax=ax)
-    ppl.plot(dthetas,dense_eps,ax=ax)
-    ppl.plot(dthetas,particle_eps,ax=ax)
+    ppl.plot(dthetas,sparse_eps,ax=ax,label='Full ADF')
+    ppl.plot(dthetas,dense_eps,ax=ax,label='Dense ADF')
+    ppl.plot(dthetas,particle_eps,ax=ax,label='Particle Filter')
+
+    ppl.legend(ax)
 
     plt.show()
---
+++ figure_5_14.py
@@ -0,0 +1,111 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+"""Particle filtering for nonlinear dynamic systems observed through adaptive poisson neurons"""
+import particlefilter as pf
+import gaussianenv as ge
+import poissonneuron as pn
+import numpy as np
+import cPickle as pic
+import multiprocessing as mp
+import brewer2mpl
+import sys
+
+#parameter definitions
+
+plotting = True
+
+
+try:
+    filename = sys.argv[1]
+except:
+    filename = 'dense_sparse.pik'
+
+
+dic = pic.load( open(filename,'r'))
+sparse_eps = dic['sparse_eps']
+dense_eps = dic['dense_eps']
+particle_eps = dic['particle_eps']
+alphas = dic['alphas']
+dthetas = dic['dthetas']
+print "ALL GOOD"
+
+from prettyplotlib import plt
+import prettyplotlib as ppl
+
+#matplotlib.rcParams['font.size']=10
+
+xs,ys = np.where(np.isnan(sparse_eps))
+
+for x,y in zip(xs,ys):
+    sparse_eps[x,y] = sparse_eps[x-1,y]
+
+max1 = np.max(sparse_eps)
+max2 = np.max(dense_eps)
+max3 = np.max(particle_eps)
+
+maxtotal = np.max([max1,max2,max3])
+
+
+min1 = np.min(sparse_eps)
+min2 = np.min(dense_eps)
+min3 = np.min(particle_eps)
+
+mintotal = np.min([min1,min2,min3])
+
+ax1 = ppl.subplot2grid((3,2),[0,0],rowspan=3)
+ax2 = ppl.subplot2grid((3,2),[0,1])
+ax3 = ppl.subplot2grid((3,2),[1,1])
+ax4 = ppl.subplot2grid((3,2),[2,1])
+
+x = np.arange(-2.0,2.0,0.01)
+
+x_density = np.exp(-x**2/(2*0.25))/np.sqrt(np.pi/2)
+
+alpha=0.5
+
+font = {'size':16}
+plt.rc('font',**font)
+
+tuning1 = 0.7*np.exp(-(x-0.8)**2/(2*alpha**2))
+tuning2 = 0.7*np.exp(-(x+0.8)**2/(2*alpha**2))
+
+ppl.plot(x,x_density,label="$P(x)$",ax=ax1)
+ppl.plot(x,tuning1,label="$\lambda^1(x)$",ax=ax1)
+ppl.plot(x,tuning2,label="$\lambda^2(x)$",ax=ax1)
+
+ax1.annotate(r'$\Delta\theta$', xy = (-0.8,0.7), xytext=(0.8,0.692), arrowprops = dict(arrowstyle='<->'))
+ax1.annotate(r'$\alpha$', xy = (-0.8-alpha,0.45), xytext=(-0.8+alpha,0.442), arrowprops = dict(arrowstyle='<->'))
+
+ax1.set_xlabel('x')
+
+ppl.legend(ax1,loc=4).get_frame().set_alpha(0.7)
+
+yellorred = brewer2mpl.get_map('YlOrRd','Sequential',9).mpl_colormap
+
+dthetas = 2*dthetas
+
+ddtheta = dthetas[1]-dthetas[0]
+dalpha = alphas[1]-alphas[0]
+
+dthetas2,alphas2 = np.meshgrid(np.arange(dthetas.min(),dthetas.max()+0.5*ddtheta,ddtheta)-ddtheta/2,
+                               np.arange(alphas.min(),alphas.max()+0.5*dalpha,dalpha)-dalpha/2)
+
+p1 = ax2.pcolormesh(dthetas2,alphas2,dense_eps.T,cmap=yellorred,rasterized=True)
+ax2.axis([dthetas2.min(),dthetas2.max(),alphas2.min(),alphas2.max()])
+p2 = ax3.pcolormesh(dthetas2,alphas2,sparse_eps.T,cmap=yellorred,rasterized=True)
+ax3.axis([dthetas2.min(),dthetas2.max(),alphas2.min(),alphas2.max()])
+p3 = ax4.pcolormesh(dthetas2,alphas2,particle_eps.T,cmap=yellorred,rasterized=True)
+ax4.axis([dthetas2.min(),dthetas2.max(),alphas2.min(),alphas2.max()])
+
+plt.colorbar(p1,ax=ax2)
+plt.colorbar(p2,ax=ax3)
+plt.colorbar(p3,ax=ax4)
+
+ax4.set_xlabel(r'$\Delta\theta$')
+ax4.set_ylabel(r'$\alpha$')
+ax3.set_ylabel(r'$\alpha$')
+ax2.set_ylabel(r'$\alpha$')
+
+[p.set_clim(vmin=mintotal,vmax=maxtotal) for p in [p1,p2,p3]]
+
+
+plt.show()
