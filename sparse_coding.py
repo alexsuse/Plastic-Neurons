dfd02eb2d9e2965aeb27cf10c0fd7d5381a7a7f0
alexsuse <alexsusemihl@gmail.com>
2014-07-15 09:45:48 +0200

    multiprocessing in sparse_coding

--- sparse_coding.py
+++ sparse_coding.py
@@ -1,41 +1,15 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 """Particle filtering for nonlinear dynamic systems observed through adaptive poisson neurons"""
-from prettyplotlib import plt
-import prettyplotlib as ppl
 import particlefilter as pf
 import gaussianenv as ge
 import poissonneuron as pn
 import numpy as np
 import cPickle as pic
+import multiprocessing as mp
+
 #parameter definitions
 
-dt = 0.001
-phi = 1.2
-zeta = 1.0
-eta = 1.0
-gamma = 1.0
-timewindow = 20000
-dm = 0.0
-tau = 1.0
-nparticles = 1000
 plotting = True
-
-#env is the "environment", that is, the true process to which we don't have access
-
-env_rng = np.random.mtrand.RandomState()
-
-env = ge.GaussianEnv(gamma=gamma,eta=eta,zeta=zeta,x0=0.0,
-                     y0=.0,L=1.0,N=1,order=1,sigma=0.1,
-                     Lx=1.0,Ly=1.0,randomstate=env_rng)
-env.reset(np.array([0.0]))
-
-#code is the population of neurons, plastic poisson neurons
-
-code_rng = np.random.mtrand.RandomState()
-
-#s is the stimulus, sps holds the spikes, rates the rates of each neuron
-#and particles give the position of the particles
-#weights gives the weights associated with each particle
 
 dthetas = np.arange(0.1,2.0,0.1)
 alphas = np.arange(0.1,2.0,0.1)
@@ -43,6 +17,63 @@ alphas = np.arange(0.1,2.0,0.1)
 sparse_eps = np.zeros((dthetas.size,alphas.size))
 dense_eps = np.zeros((dthetas.size,alphas.size))
 particle_eps = np.zeros((dthetas.size,alphas.size))
+
+def run_filters(args):
+
+    i,j,dtheta,alpha = args
+    dt = 0.001
+    phi = 1.0
+    zeta = 1.0
+    eta = 1.0
+    gamma = 1.0
+    timewindow = 20000
+    dm = 0.0
+    tau = 1.0
+    nparticles = 500
+    plotting = True
+
+    #env is the "environment", that is, the true process to which we don't have access
+
+    env_rng = np.random.mtrand.RandomState()
+
+    env = ge.GaussianEnv(gamma=gamma,eta=eta,zeta=zeta,x0=0.0,
+                         y0=.0,L=1.0,N=1,order=1,sigma=0.1,
+                         Lx=1.0,Ly=1.0,randomstate=env_rng)
+    env.reset(np.array([0.0]))
+
+    #code is the population of neurons, plastic poisson neurons
+
+    code_rng = np.random.mtrand.RandomState()
+
+    code = pn.PoissonPlasticCode(phi=phi,tau=tau,alpha=alpha
+                                 thetas=np.arange(-dtheta,dtheta+0.1,2*dtheta),
+                                 dm=dm,randomstate=code_rng)
+    
+    env_rng.seed(12345)
+    code_rng.seed(67890)
+    env.reset(np.array([0.0]))
+    code.reset()
+    
+    [densem,densevar,spsg,sg,dense_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
+                                                            dense=True)
+    
+    env_rng.seed(12345)
+    code_rng.seed(67890)
+    env.reset(np.array([0.0]))
+    code.reset()
+    
+    [sparsem,sparsevar,spsg,sg,sparse_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
+                                                                dense=False)
+    
+    env_rng.seed(12345)
+    code_rng.seed(67890)
+    env.reset(np.array([0.0]))
+    code.reset()
+    
+    [msep,ws] = pf.mse_particle_filter(code, env, timewindow=timewindow,
+                                                         dt=dt, nparticles=nparticles,
+                                                         testf=(lambda x:x))
+    return i,j,dense_mse,sparse_mse,particle_mse
 
 try:
     dic = pic.load( open('dense_sparse.pik','r'))
@@ -55,42 +86,23 @@ try:
 
 except:
 
+    args = []
+
     for i,dtheta in enumerate(dthetas):
         for j,alpha in enumerate(alphas):
-            print i,dthetas.size,j,alphas.size
-            
-            code = pn.PoissonPlasticCode(A=alpha,phi=phi/2,tau=tau,
-                                         thetas=np.arange(-dtheta,dtheta+0.1,2*dtheta),
-                                         dm=dm,randomstate=code_rng)
-            
-            env_rng.seed(12345)
-            code_rng.seed(67890)
-            env.reset(np.array([0.0]))
-            code.reset()
-            
-            [densem,densevar,spsg,sg,dense_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
-                                                                    dense=True)
-            
-            env_rng.seed(12345)
-            code_rng.seed(67890)
-            env.reset(np.array([0.0]))
-            code.reset()
-            
-            [sparsem,sparsevar,spsg,sg,sparse_mse] = pf.gaussian_filter(code,env,timewindow=timewindow,dt=dt,
-                                                                        dense=False)
-            
-            env_rng.seed(12345)
-            code_rng.seed(67890)
-            env.reset(np.array([0.0]))
-            code.reset()
-            
-            [msep,ws] = pf.mse_particle_filter(code, env, timewindow=timewindow,
-                                                                 dt=dt, nparticles=nparticles,
-                                                                 testf=(lambda x:x))
+            args.append((i,j,dtheta,alpha))
 
-            dense_eps[i,j] = dense_mse
-            sparse_eps[i,j] = sparse_mse
-            particle_eps[i,j] = msep
+    p = mp.Pool(mp.cpu_count())
+
+    results = p.map(run_filters,args)
+
+    for res in results:
+        i,j,dense_mse,sparse_mse,msep = res
+
+        dense_eps[i,j] = dense_mse
+        sparse_eps[i,j] = sparse_mse
+        particle_eps[i,j] = msep
+
     with open("dense_sparse.pik","wb") as f:
         pic.dump({'dense_eps':dense_eps,
                   'sparse_eps':sparse_eps,
@@ -100,6 +112,8 @@ except:
                   f)
 
 if plotting:
+    from prettyplotlib import plt
+    import prettyplotlib as ppl
     
     #matplotlib.rcParams['font.size']=10
    
