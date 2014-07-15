d95975650d9e3d33abcb95b2993a458ea3588002
alexsuse <alexsusemihl@gmail.com>
2014-07-15 23:18:32 +0200

    good figurez

--- figure_5_14.py
+++ figure_5_14.py
@@ -52,9 +52,9 @@ min3 = np.min(particle_eps)
 mintotal = np.min([min1,min2,min3])
 
 ax1 = ppl.subplot2grid((3,2),[0,0],rowspan=3)
+ax4 = ppl.subplot2grid((3,2),[2,1])
 ax2 = ppl.subplot2grid((3,2),[0,1])
 ax3 = ppl.subplot2grid((3,2),[1,1])
-ax4 = ppl.subplot2grid((3,2),[2,1])
 
 x = np.arange(-2.0,2.0,0.01)
 
@@ -62,7 +62,7 @@ x_density = np.exp(-x**2/(2*0.25))/np.sqrt(np.pi/2)
 
 alpha=0.5
 
-font = {'size':16}
+font = {'size':10}
 plt.rc('font',**font)
 
 tuning1 = 0.7*np.exp(-(x-0.8)**2/(2*alpha**2))
@@ -96,16 +96,34 @@ ax3.axis([dthetas2.min(),dthetas2.max(),alphas2.min(),alphas2.max()])
 p3 = ax4.pcolormesh(dthetas2,alphas2,particle_eps.T,cmap=yellorred,rasterized=True)
 ax4.axis([dthetas2.min(),dthetas2.max(),alphas2.min(),alphas2.max()])
 
-plt.colorbar(p1,ax=ax2)
-plt.colorbar(p2,ax=ax3)
-plt.colorbar(p3,ax=ax4)
+ticks = np.array([mintotal,(mintotal+maxtotal)/2,maxtotal])
+ticklabels = np.round(ticks,decimals=2)
+
+cb1 = plt.colorbar(p1,ax=ax2)
+cb1.set_ticks(ticks)
+cb1.set_ticklabels(ticklabels)
+cb2 = plt.colorbar(p2,ax=ax3)
+cb2.set_ticks(ticks)
+cb2.set_ticklabels(ticklabels)
+cb3 = plt.colorbar(p3,ax=ax4)
+cb3.set_ticks(ticks)
+cb3.set_ticklabels(ticklabels)
 
 ax4.set_xlabel(r'$\Delta\theta$')
 ax4.set_ylabel(r'$\alpha$')
 ax3.set_ylabel(r'$\alpha$')
 ax2.set_ylabel(r'$\alpha$')
 
+ax3.axes.get_xaxis().set_ticks([])
+ax2.axes.get_xaxis().set_ticks([])
+
+#ax3.tick_params(axis='x',which='both',bottom='off')
+#ax2.tick_params(axis='x',which='both',bottom='off')
+
+ax2.set_title('ADF with dense coding assumption')
+ax3.set_title('Full ADF')
+ax4.set_title('Particle Filter')
+
 [p.set_clim(vmin=mintotal,vmax=maxtotal) for p in [p1,p2,p3]]
 
-
-plt.show()
+plt.savefig('figure_5_14.pdf')
